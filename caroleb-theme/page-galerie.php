3899a53ad9ee5b61f1bd2b31dc4e6ba02c7325cb
Leogeox <leovginx@gmail.com>
2025-12-04 12:27:17 +0100

    feat(Leo): added artworks on galerie.php using post_type and field_group

--- caroleb-theme/page-galerie.php
+++ caroleb-theme/page-galerie.php
@@ -27,6 +27,34 @@ get_header();
     </nav>
 
     <section class="galerie-wrapper">
+        <?php
+
+        $posts = get_posts(array(
+            'posts_per_page' => -1,
+            'post_type' => 'oeuvre'
+        ));
+
+        if ($posts): ?>
+            <ul>
+                <?php foreach ($posts as $post):
+                    setup_postdata($post);
+                    ?>
+                    <li>
+                        <?php
+                        $image = get_field('image');
+                        $size = 'medium';
+                        ?>
+                        <a href="
+                        <?php the_permalink(); ?>"><?php if ($image) {
+                              echo wp_get_attachment_image($image, $size);
+                          } ?>
+                        </a>
+                    </li>
+                <?php endforeach; ?>
+            </ul>
+            <?php wp_reset_postdata(); ?>
+        <?php endif; ?>
+
     </section>
 
     <!-- <nav class="container"></nav> -->
