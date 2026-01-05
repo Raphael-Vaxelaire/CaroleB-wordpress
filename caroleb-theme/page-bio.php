7f20452def6c1fd1f3007509083bb62e5d595921
AilesVax <raphael.vaxelaire10@gmail.com>
2026-01-05 12:14:46 +0100

    feat(raph) page home + page bio finis

--- caroleb-theme/page-bio.php
+++ caroleb-theme/page-bio.php
@@ -2,7 +2,41 @@
 
 get_header();
 
-the_title();
+
 ?>
+
+ <?php
+        $query = new WP_Query(array(
+            'post_type' => 'post-bio',
+            'p' => 274
+        ));
+
+        if ($query->have_posts()):
+            while ($query->have_posts()):
+                $query->the_post();
+                
+                the_content();
+            endwhile;
+        endif;
+
+        wp_reset_postdata();
+        ?>
+
+         <?php
+        $query = new WP_Query(array(
+            'post_type' => 'post-bio',
+            'p' => 275
+        ));
+
+        if ($query->have_posts()):
+            while ($query->have_posts()):
+                $query->the_post();
+                
+                the_content();
+            endwhile;
+        endif;
+
+        wp_reset_postdata();
+        ?>
 <?php
 get_footer();
