f505f4a635f94cd541d1fc15ddf948b5407c0838
Leogeox <leovginx@gmail.com>
2026-01-07 19:42:34 +0100

    style(Leo): reponsive homepage, galerie and press

--- caroleb-theme/page-presse.php
+++ caroleb-theme/page-presse.php
@@ -5,8 +5,8 @@ get_header();
 <main id="main-content" class="site-main">
     <section class="media-intro">
         <h1 class="presse-title"> <?php the_title(); ?></h1>
-        <?php the_content(); ?>
     </section>
+    <?php the_content(); ?>
 </main>
 
 <?php
